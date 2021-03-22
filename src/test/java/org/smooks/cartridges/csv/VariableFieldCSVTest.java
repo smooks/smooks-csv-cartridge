8d1b6b3eb5da708c3700cf6c44c09671a1e4d071
claudemamo <823038+claudemamo@users.noreply.github.com>
2021-03-22 21:26:29 +0100

    fix: solve compilation errors following breaking changes in smooks/smooks#439

--- src/test/java/org/smooks/cartridges/csv/VariableFieldCSVTest.java
+++ src/test/java/org/smooks/cartridges/csv/VariableFieldCSVTest.java
@@ -46,8 +46,8 @@ import org.custommonkey.xmlunit.XMLAssert;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Test;
 import org.smooks.Smooks;
-import org.smooks.io.StreamUtils;
 import org.smooks.io.payload.StringResult;
+import org.smooks.support.StreamUtils;
 import org.xml.sax.SAXException;
 
 import javax.xml.transform.stream.StreamSource;

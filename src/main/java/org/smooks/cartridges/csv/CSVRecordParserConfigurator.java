2edb7e3a2f07efd7767596e8ae34ee0351b827cc
Claude Mamo <823038+claudemamo@users.noreply.github.com>
2024-03-09 19:15:43 +0100

    chore: prepare for development (#118)
    update to latest Smooks snapshot dependencies

--- src/main/java/org/smooks/cartridges/csv/CSVRecordParserConfigurator.java
+++ src/main/java/org/smooks/cartridges/csv/CSVRecordParserConfigurator.java
@@ -6,35 +6,35 @@
  * %%
  * Licensed under the terms of the Apache License Version 2.0, or
  * the GNU Lesser General Public License version 3.0 or later.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
- * 
+ *
  * ======================================================================
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ======================================================================
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -53,7 +53,7 @@ import java.util.List;
  * <p/>
  * Supports programmatic configuration of {@link CSVRecordParserFactory} and
  * {@link CSVRecordParser}.
- * 
+ *
  * @author <a href="mailto:tom.fennelly@jboss.com">tom.fennelly@jboss.com</a>
  */
 public class CSVRecordParserConfigurator extends VariableFieldRecordParserConfigurator {
@@ -61,7 +61,7 @@ public class CSVRecordParserConfigurator extends VariableFieldRecordParserConfig
     private String csvFields;
     private char separatorChar = ',';
     private char quoteChar = '"';
-	private char escapeChar = '\\';
+    private char escapeChar = '\\';
     private int skipLineCount = 0;
     private String rootElementName = "csv-set";
     private String recordElementName = "csv-record";
@@ -83,8 +83,8 @@ public class CSVRecordParserConfigurator extends VariableFieldRecordParserConfig
         this.quoteChar = quoteChar;
         return this;
     }
-	
-	public CSVRecordParserConfigurator setEscapeChar(char escapeChar) {
+
+    public CSVRecordParserConfigurator setEscapeChar(char escapeChar) {
         AssertArgument.isNotNull(escapeChar, "escapeChar");
         this.escapeChar = escapeChar;
         return this;
@@ -112,7 +112,7 @@ public class CSVRecordParserConfigurator extends VariableFieldRecordParserConfig
         getParameters().setProperty("fields", csvFields);
         getParameters().setProperty("separator", Character.toString(separatorChar));
         getParameters().setProperty("quote-char", Character.toString(quoteChar));
-		getParameters().setProperty("escape-char", Character.toString(escapeChar));
+        getParameters().setProperty("escape-char", Character.toString(escapeChar));
         getParameters().setProperty("skip-line-count", Integer.toString(skipLineCount));
         getParameters().setProperty("rootElementName", rootElementName);
         getParameters().setProperty("recordElementName", recordElementName);
--- src/test/java/org/smooks/cartridges/csv/MILYN_642/MILYN_642_Test.java
+++ src/test/java/org/smooks/cartridges/csv/MILYN_642/MILYN_642_Test.java
@@ -6,35 +6,35 @@
  * %%
  * Licensed under the terms of the Apache License Version 2.0, or
  * the GNU Lesser General Public License version 3.0 or later.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
- * 
+ *
  * ======================================================================
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ======================================================================
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -58,7 +58,7 @@ import static org.junit.Assert.assertEquals;
  * @author Ken Hill
  */
 public class MILYN_642_Test {
-	// Default escape char
+    // Default escape char
     @Test
     public void test_01() throws SmooksException, IOException, SAXException {
         Smooks smooks = new Smooks(getClass().getResourceAsStream("/MILYN_642/test-01-config.xml"));
@@ -67,11 +67,11 @@ public class MILYN_642_Test {
         String result = SmooksUtil.filterAndSerialize(context, getClass().getResourceAsStream("/MILYN_642/test-01-data.csv"), smooks);
 
         assertEquals(
-            "<csv-set><csv-record number=\"1\"><name>Erika Mustermann</name><email>e.m@ex.org</email></csv-record><csv-record number=\"2\"><name>Max \"The Man\" Mustermann</name><email>m.m@ex.org</email></csv-record></csv-set>",
-            result);
+                "<csv-set><csv-record number=\"1\"><name>Erika Mustermann</name><email>e.m@ex.org</email></csv-record><csv-record number=\"2\"><name>Max \"The Man\" Mustermann</name><email>m.m@ex.org</email></csv-record></csv-set>",
+                result);
     }
 
-	// Custom escape char, xml config
+    // Custom escape char, xml config
     @Test
     public void test_02() throws SmooksException, IOException, SAXException {
         Smooks smooks = new Smooks(getClass().getResourceAsStream("/MILYN_642/test-02-config.xml"));
@@ -80,24 +80,24 @@ public class MILYN_642_Test {
         String result = SmooksUtil.filterAndSerialize(context, getClass().getResourceAsStream("/MILYN_642/test-02-data.csv"), smooks);
 
         assertEquals(
-            "<csv-set><csv-record number=\"1\"><name>Erika Mustermann</name><email>e.m@ex.org</email></csv-record><csv-record number=\"2\"><name>Max \"The Man\" Mustermann</name><email>m.m@ex.org</email></csv-record></csv-set>",
-            result);
+                "<csv-set><csv-record number=\"1\"><name>Erika Mustermann</name><email>e.m@ex.org</email></csv-record><csv-record number=\"2\"><name>Max \"The Man\" Mustermann</name><email>m.m@ex.org</email></csv-record></csv-set>",
+                result);
     }
-	
-	// Custom escape, programmatic config
+
+    // Custom escape, programmatic config
     @Test
     public void test_03() throws SmooksException, IOException, SAXException {
         Smooks smooks = new Smooks();
-		
+
         smooks.setReaderConfig(
-            new CSVRecordParserConfigurator("name,email")
-                .setEscapeChar('~'));
+                new CSVRecordParserConfigurator("name,email")
+                        .setEscapeChar('~'));
 
         ExecutionContext context = smooks.createExecutionContext();
         String result = SmooksUtil.filterAndSerialize(context, getClass().getResourceAsStream("/MILYN_642/test-02-data.csv"), smooks);
-		
+
         assertEquals(
-            "<csv-set><csv-record number=\"1\"><name>Erika Mustermann</name><email>e.m@ex.org</email></csv-record><csv-record number=\"2\"><name>Max \"The Man\" Mustermann</name><email>m.m@ex.org</email></csv-record></csv-set>",
-            result);
+                "<csv-set><csv-record number=\"1\"><name>Erika Mustermann</name><email>e.m@ex.org</email></csv-record><csv-record number=\"2\"><name>Max \"The Man\" Mustermann</name><email>m.m@ex.org</email></csv-record></csv-set>",
+                result);
     }
 }

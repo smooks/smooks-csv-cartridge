9dc81dbb02c3391e81a04a12ba25f0e6a6b2fff9
cjmamo <823038+claudemamo@users.noreply.github.com>
2024-03-09 19:12:18 +0100

    chore: prepare for development
    update to latest Smooks snapshot dependencies

--- src/test/java/org/smooks/cartridges/csv/CSVReaderTest.java
+++ src/test/java/org/smooks/cartridges/csv/CSVReaderTest.java
@@ -6,35 +6,35 @@
  * %%
  * Licensed under the terms of the Apache License Version 2.0, or
  * the GNU Lesser General Public License version 3.0 or later.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
- * 
+ *
  * ======================================================================
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ======================================================================
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -113,8 +113,7 @@ public class CSVReaderTest {
     }
 
     @Test
-    public void test_07() throws SmooksException
-    {
+    public void test_07() throws SmooksException {
         Smooks smooks = new Smooks();
 
         smooks.setReaderConfig(new CSVRecordParserConfigurator("firstname,lastname,gender,age,country"));
@@ -128,8 +127,7 @@ public class CSVReaderTest {
     }
 
     @Test
-    public void test_08() throws SmooksException
-    {
+    public void test_08() throws SmooksException {
         Smooks smooks = new Smooks();
 
         smooks.setReaderConfig(new CSVRecordParserConfigurator("firstname,lastname,gender,age,country")
@@ -169,8 +167,7 @@ public class CSVReaderTest {
     }
 
     @Test
-    public void test_10() throws SmooksException
-    {
+    public void test_10() throws SmooksException {
         Smooks smooks = new Smooks();
 
         smooks.setReaderConfig(new CSVRecordParserConfigurator("firstname,lastname,$ignore$,gender,age,country")
@@ -186,8 +183,7 @@ public class CSVReaderTest {
     }
 
     @Test
-    public void test_11() throws SmooksException
-    {
+    public void test_11() throws SmooksException {
         Smooks smooks = new Smooks();
 
         smooks.setReaderConfig(new CSVRecordParserConfigurator("firstname,lastname,$ignore$,gender,age,country")
@@ -201,8 +197,7 @@ public class CSVReaderTest {
     }
 
     @Test
-    public void test_12() throws SmooksException
-    {
+    public void test_12() throws SmooksException {
         Smooks smooks = new Smooks();
 
         smooks.setReaderConfig(new CSVRecordParserConfigurator("firstname,lastname,$ignore$,gender,age,country")
@@ -237,7 +232,7 @@ public class CSVReaderTest {
         assertEquals("22", person.get("age"));
         assertEquals("Ireland", person.get("country"));
     }
-    
+
 
     @Test
     public void test_13_xml() throws SmooksException, IOException, SAXException {
@@ -277,8 +272,7 @@ public class CSVReaderTest {
     }
 
     @Test
-    public void test_14_programmatic() throws SmooksException
-    {
+    public void test_14_programmatic() throws SmooksException {
         Smooks smooks = new Smooks();
 
         smooks.setReaderConfig(new CSVRecordParserConfigurator("firstname,lastname,$ignore$,gender,age,country")
@@ -379,8 +373,7 @@ public class CSVReaderTest {
     }
 
     @Test
-    public void test_18() throws SmooksException
-    {
+    public void test_18() throws SmooksException {
         Smooks smooks = new Smooks();
 
         smooks.setReaderConfig(new CSVRecordParserConfigurator("firstname?upper_case,lastname?uncap_first,$ignore$5")
@@ -442,8 +435,7 @@ public class CSVReaderTest {
     }
 
     @Test
-    public void test_null_field_values() throws SmooksException
-    {
+    public void test_null_field_values() throws SmooksException {
         Smooks smooks = new Smooks();
 
         smooks.setReaderConfig(new CSVRecordParserConfigurator("firstname,lastname,gender,age,country"));
--- src/test/java/org/smooks/cartridges/csv/MILYN_428/MILYN_428_Test.java
+++ src/test/java/org/smooks/cartridges/csv/MILYN_428/MILYN_428_Test.java
@@ -6,35 +6,35 @@
  * %%
  * Licensed under the terms of the Apache License Version 2.0, or
  * the GNU Lesser General Public License version 3.0 or later.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
- * 
+ *
  * ======================================================================
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ======================================================================
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -57,118 +57,117 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 /**
- * 
  * @author Clemens Fuchslocher
  */
 public class MILYN_428_Test {
 
-	// No errors.
-	@Test
-	public void test01() throws IOException, SAXException {
-		Smooks smooks = null;
-		try {
-			smooks = new Smooks(getConfig("/MILYN_428/test-01-config.xml"));
-			smooks.filterSource(getSource("/MILYN_428/test-01-data.csv"));
-		} catch (SmooksException exception) {
-			fail(getStackTrace(exception));
-		} finally {
-			if (smooks != null) {
-				smooks.close();
-			}
-		}
-	}
-	
-	// Ignored fields.
-	@Test
-	public void test04() throws IOException, SAXException {
-		Smooks smooks = null;
-		try {
-			smooks = new Smooks(getConfig("/MILYN_428/test-04-config.xml"));
-			smooks.filterSource(getSource("/MILYN_428/test-04-data.csv"));
-		} catch (SmooksException exception) {
-			fail(getStackTrace(exception));
-		} finally {
-			if (smooks != null) {
-				smooks.close();
-			}
-		}
-	}
+    // No errors.
+    @Test
+    public void test01() throws IOException, SAXException {
+        Smooks smooks = null;
+        try {
+            smooks = new Smooks(getConfig("/MILYN_428/test-01-config.xml"));
+            smooks.filterSource(getSource("/MILYN_428/test-01-data.csv"));
+        } catch (SmooksException exception) {
+            fail(getStackTrace(exception));
+        } finally {
+            if (smooks != null) {
+                smooks.close();
+            }
+        }
+    }
 
-	// String manipulation functions.
-	@Test
-	public void test05() throws IOException, SAXException {
-		Smooks smooks = null;
-		try {
-			smooks = new Smooks(getConfig("/MILYN_428/test-05-config.xml"));
-			smooks.filterSource(getSource("/MILYN_428/test-05-data.csv"));
-		} catch (SmooksException exception) {
-			fail(getStackTrace(exception));
-		} finally {
-			if (smooks != null) {
-				smooks.close();
-			}
-		}
-	}
+    // Ignored fields.
+    @Test
+    public void test04() throws IOException, SAXException {
+        Smooks smooks = null;
+        try {
+            smooks = new Smooks(getConfig("/MILYN_428/test-04-config.xml"));
+            smooks.filterSource(getSource("/MILYN_428/test-04-data.csv"));
+        } catch (SmooksException exception) {
+            fail(getStackTrace(exception));
+        } finally {
+            if (smooks != null) {
+                smooks.close();
+            }
+        }
+    }
 
-	// No header validation.
-	@Test
-	public void test08() throws IOException, SAXException {
-		Smooks smooks = null;
-		try {
-			smooks = new Smooks(getConfig("/MILYN_428/test-08-config.xml"));
-			smooks.filterSource(getSource("/MILYN_428/test-08-data.csv"));
-		} catch (SmooksException exception) {
-			fail(getStackTrace(exception));
-		} finally {
-			if (smooks != null) {
-				smooks.close();
-			}
-		}
-	}
+    // String manipulation functions.
+    @Test
+    public void test05() throws IOException, SAXException {
+        Smooks smooks = null;
+        try {
+            smooks = new Smooks(getConfig("/MILYN_428/test-05-config.xml"));
+            smooks.filterSource(getSource("/MILYN_428/test-05-data.csv"));
+        } catch (SmooksException exception) {
+            fail(getStackTrace(exception));
+        } finally {
+            if (smooks != null) {
+                smooks.close();
+            }
+        }
+    }
 
-	// Disabled header validation.
-	@Test
-	public void test09() throws IOException, SAXException {
-		Smooks smooks = null;
-		try {
-			smooks = new Smooks(getConfig("/MILYN_428/test-09-config.xml"));
-			smooks.filterSource(getSource("/MILYN_428/test-09-data.csv"));
-		} catch (SmooksException exception) {
-			fail(getStackTrace(exception));
-		} finally {
-			if (smooks != null) {
-				smooks.close();
-			}
-		}
-	}
+    // No header validation.
+    @Test
+    public void test08() throws IOException, SAXException {
+        Smooks smooks = null;
+        try {
+            smooks = new Smooks(getConfig("/MILYN_428/test-08-config.xml"));
+            smooks.filterSource(getSource("/MILYN_428/test-08-data.csv"));
+        } catch (SmooksException exception) {
+            fail(getStackTrace(exception));
+        } finally {
+            if (smooks != null) {
+                smooks.close();
+            }
+        }
+    }
 
-	private InputStream getConfig(final String file) throws FileNotFoundException {
-		return getClass().getResourceAsStream(file);
-	}
+    // Disabled header validation.
+    @Test
+    public void test09() throws IOException, SAXException {
+        Smooks smooks = null;
+        try {
+            smooks = new Smooks(getConfig("/MILYN_428/test-09-config.xml"));
+            smooks.filterSource(getSource("/MILYN_428/test-09-data.csv"));
+        } catch (SmooksException exception) {
+            fail(getStackTrace(exception));
+        } finally {
+            if (smooks != null) {
+                smooks.close();
+            }
+        }
+    }
 
-	private Source getSource(final String file) throws FileNotFoundException {
-		return new StreamSource(getClass().getResourceAsStream(file));
-	}
+    private InputStream getConfig(final String file) throws FileNotFoundException {
+        return getClass().getResourceAsStream(file);
+    }
 
-	public static String getStackTrace(final Throwable throwable) {
-		Writer writer = new StringWriter();
-		throwable.printStackTrace(new PrintWriter(writer));
-		return writer.toString();
-	}
+    private Source getSource(final String file) throws FileNotFoundException {
+        return new StreamSource(getClass().getResourceAsStream(file));
+    }
 
-	private void assertException(final Exception exception, final String[] expected, final String[] found, final String message) {
-		Throwable cause = exception.getCause();
-		if (cause == null) {
-			fail("cause == null");
-		}
+    public static String getStackTrace(final Throwable throwable) {
+        Writer writer = new StringWriter();
+        throwable.printStackTrace(new PrintWriter(writer));
+        return writer.toString();
+    }
 
-		if (!(cause instanceof CSVHeaderValidationException)) {
-			fail("!(cause instanceof CsvHeaderValidationException)");
-		}
+    private void assertException(final Exception exception, final String[] expected, final String[] found, final String message) {
+        Throwable cause = exception.getCause();
+        if (cause == null) {
+            fail("cause == null");
+        }
 
-		CSVHeaderValidationException validation = (CSVHeaderValidationException) cause;
-		assertEquals(Arrays.asList(expected), validation.getExpected());
-		assertEquals(Arrays.asList(found), validation.getFound());
-		assertEquals(message, validation.getMessage());
-	}
+        if (!(cause instanceof CSVHeaderValidationException)) {
+            fail("!(cause instanceof CsvHeaderValidationException)");
+        }
+
+        CSVHeaderValidationException validation = (CSVHeaderValidationException) cause;
+        assertEquals(Arrays.asList(expected), validation.getExpected());
+        assertEquals(Arrays.asList(found), validation.getFound());
+        assertEquals(message, validation.getMessage());
+    }
 }
